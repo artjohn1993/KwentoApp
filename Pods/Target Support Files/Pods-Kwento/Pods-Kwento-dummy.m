6384718625ff7ee476f72f3ee9ccf76407ec6f32
Tadashi Yamada <artjohn1993@gmail.com>
2020-01-08 16:42:46 +0800

    Add files

--- Pods/Target Support Files/Pods-Kwento/Pods-Kwento-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Kwento : NSObject
-@end
-@implementation PodsDummy_Pods_Kwento
-@end
--- Pods/Target Support Files/Pods-KwentoTests/Pods-KwentoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KwentoTests : NSObject
-@end
-@implementation PodsDummy_Pods_KwentoTests
-@end
